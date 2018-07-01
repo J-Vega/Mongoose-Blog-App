4f47d49197811bbaa7ef66c15984fc83be516bb0
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-06-30 20:42:50 -0400

    added comment showing the correct command to add blog data to a database

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//mongoimport --db blog-app --collection blogPosts --drop --file ~/Desktop/Projects/Blog-API-Mongoose/seed-data.json
+
 //	Allows us to easily configure values for the Mongo database URL 
 //	and the port the application will run on.
 exports.DATABASE_URL = process.env.DATABASE_URL ||
