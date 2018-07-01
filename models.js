02b0b8a7a9b764c8e923e95b92487482047bf1f9
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-06-30 21:01:50 -0400

    added global promise, author name trim function, and return schema method

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
 const blogSchema = mongoose.Schema({
 
@@ -10,6 +11,20 @@ const blogSchema = mongoose.Schema({
 
 	created: {type: Date, default: Date.now}
 });
+
+blogSchema.virtual('authorName').get(function(){
+	return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogSchema.methods.serialize = function() {
+	return {
+		title: this.title,
+		author: this.authorName,
+		content: this.content,
+		id: this._id,
+		created: this.created
+	};
+};
 
 const BlogPost = mongoose.model('Blogpost',blogSchema)
 
